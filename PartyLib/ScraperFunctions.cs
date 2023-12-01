ee3ec2fd3c4a3c349b1430e8936d08c28c4a2481
brandenstober <brandenstober@outlook.com>
2023-12-01 15:37:12 -0500

    Cleaned up ScraperFunctions.cs

--- PartyLib/ScraperFunctions.cs
+++ PartyLib/ScraperFunctions.cs
@@ -47,96 +47,6 @@ public class ScraperFunctions
     public int TotalRequestedPosts { get; set; }
 
     /// <summary>
-    /// Does math to separate pages from a number of posts. 0 can be input to process all of the
-    /// creator's posts
-    /// </summary>
-    /// <returns>A PageDetails class</returns>
-    public PageDetails DoPageMath()
-    {
-        if (TotalRequestedPosts != 0)
-        {
-            // Posts integer is a defined value, so separation code goes here
-            if (TotalRequestedPosts <= 50)
-            {
-                // Posts integer only requires 1 page
-                return new PageDetails(0, TotalRequestedPosts, true);
-            }
-            // Posts integer requires more than 1 page
-            return new PageDetails((int)Math.Floor((float)(TotalRequestedPosts / 50)), TotalRequestedPosts % 50, false);
-        }
-
-        var totalPosts = Creator.GetTotalPosts();
-        if (totalPosts != -1)
-        {
-            return new PageDetails((int)Math.Floor((float)(totalPosts / 50)), totalPosts % 50, false);
-        }
-
-        // Total posts element doesn't appear if there is only 1 page, so that logic is handled here
-        var posts = new List<HtmlNode>();
-        var postsList =
-            Creator.LandingPage.DocumentNode.SelectSingleNode("/html/body/div[2]/main/section/div[3]/div[2]");
-        foreach (var post in postsList.ChildNodes)
-        {
-            if (post.NodeType == HtmlNodeType.Element)
-            {
-                posts.Add(post);
-            }
-        }
-        return new PageDetails(0, posts.Count, true);
-    }
-
-    /// <summary>
-    /// Scrapes a creator's page for posts.
-    /// </summary>
-    /// <param name="page"></param>
-    /// <param name="numberOfPostsToGet"></param>
-    /// <returns>A list of posts</returns>
-    public List<Post> ScrapePage(int page, int numberOfPostsToGet)
-    {
-        var postUrls = new List<Post>();
-        var client = new RestClient(Creator.URL);
-        var request = new RestRequest().AddParameter("o", page * 50); // Page parameter calculation
-
-        // Headers
-        request.AddHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
-        request.AddHeader("Accept-Encoding", "gzip, deflate, br");
-        request.AddHeader("Accept-Language", "en-US,en;q=0.5");
-        request.AddHeader("Connection", "keep-alive");
-        request.AddHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0");
-
-        // HTTP query
-        var response = client.GetAsync(request).Result;
-        var htmlDoc = new HtmlDocument();
-        htmlDoc.LoadHtml(response.Content);
-
-        // Parsing
-        var postsContainer = htmlDoc.DocumentNode.Descendants().FirstOrDefault(x => x.HasClass("card-list__items") && x.Name == "div"); // Fetch the container that holds the posts
-        var count = 0;
-        if (postsContainer?.ChildNodes != null)
-        {
-            if (postsContainer?.ChildNodes != null)
-            {
-                foreach (var post in postsContainer?.ChildNodes)
-                {
-                    if (post.NodeType != HtmlNodeType.Element) // We don't want text or comments
-                        continue;
-
-                    if (count >= numberOfPostsToGet) // We want to cut off the loop when the threshold is reached
-                        break;
-
-                    var linkNode = post.ChildNodes.FirstOrDefault(x => x.Attributes["href"] != null); // Find first child node with a link attribute (only "a" nodes here)
-
-                    // Add URL to the list
-                    postUrls.Add(ScrapePost(Creator.PartyDomain + linkNode?.Attributes["href"].Value, (page * 50) + count));
-                    count++;
-                }
-            }
-        }
-
-        return postUrls;
-    }
-
-    /// <summary>
     /// Raw download function for interacting with the downloader library
     /// </summary>
     /// <param name="url"></param>
@@ -226,24 +136,6 @@ public class ScraperFunctions
     }
 
     /// <summary>
-    /// Downloads the specified attachment. Basically just a public wrapper.
-    /// </summary>
-    /// <param name="attachment"></param>
-    /// <param name="parentFolder"></param>
-    /// <returns>A boolean representing the success of the download</returns>
-    public bool DownloadAttachment(Attachment attachment, string parentFolder)
-    {
-        if (attachment is { URL: not null, FileName: not null })
-        {
-            return DownloadContent(attachment.URL, parentFolder, attachment.FileName);
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    /// <summary>
     /// Scrapes a post and returns an assembled class
     /// </summary>
     /// <param name="postUrl"></param>
@@ -262,5 +154,114 @@ public class ScraperFunctions
         htmlDoc.LoadHtml(response.Content);
 
         return post;
+    }
+
+    /// <summary>
+    /// Does math to separate pages from a number of posts. 0 can be input to process all of the
+    /// creator's posts
+    /// </summary>
+    /// <returns>A PageDetails class</returns>
+    public PageDetails DoPageMath()
+    {
+        if (TotalRequestedPosts != 0)
+        {
+            // Posts integer is a defined value, so separation code goes here
+            if (TotalRequestedPosts <= 50)
+            {
+                // Posts integer only requires 1 page
+                return new PageDetails(0, TotalRequestedPosts, true);
+            }
+            // Posts integer requires more than 1 page
+            return new PageDetails((int)Math.Floor((float)(TotalRequestedPosts / 50)), TotalRequestedPosts % 50, false);
+        }
+
+        var totalPosts = Creator.GetTotalPosts();
+        if (totalPosts != -1)
+        {
+            return new PageDetails((int)Math.Floor((float)(totalPosts / 50)), totalPosts % 50, false);
+        }
+
+        // Total posts element doesn't appear if there is only 1 page, so that logic is handled here
+        var posts = new List<HtmlNode>();
+        var postsList =
+            Creator.LandingPage.DocumentNode.SelectSingleNode("/html/body/div[2]/main/section/div[3]/div[2]");
+        foreach (var post in postsList.ChildNodes)
+        {
+            if (post.NodeType == HtmlNodeType.Element)
+            {
+                posts.Add(post);
+            }
+        }
+        return new PageDetails(0, posts.Count, true);
+    }
+
+    /// <summary>
+    /// Scrapes a creator's page for posts.
+    /// </summary>
+    /// <param name="page"></param>
+    /// <param name="numberOfPostsToGet"></param>
+    /// <returns>A list of posts</returns>
+    public List<Post> ScrapePage(int page, int numberOfPostsToGet)
+    {
+        var postUrls = new List<Post>();
+        var client = new RestClient(Creator.URL);
+        var request = new RestRequest().AddParameter("o", page * 50); // Page parameter calculation
+
+        // Headers
+        string randomUA = RandomUa.RandomUserAgent;
+        request.AddHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
+        request.AddHeader("Accept-Encoding", "gzip, deflate, br");
+        request.AddHeader("Accept-Language", "en-US,en;q=0.5");
+        request.AddHeader("Connection", "keep-alive");
+        request.AddHeader("User-Agent", randomUA);
+
+        // HTTP query
+        var response = client.GetAsync(request).Result;
+        var htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(response.Content);
+
+        // Parsing
+        var postsContainer = htmlDoc.DocumentNode.Descendants().FirstOrDefault(x => x.HasClass("card-list__items") && x.Name == "div"); // Fetch the container that holds the posts
+        var count = 0;
+        if (postsContainer?.ChildNodes != null)
+        {
+            if (postsContainer?.ChildNodes != null)
+            {
+                foreach (var post in postsContainer?.ChildNodes)
+                {
+                    if (post.NodeType != HtmlNodeType.Element) // We don't want text or comments
+                        continue;
+
+                    if (count >= numberOfPostsToGet) // We want to cut off the loop when the threshold is reached
+                        break;
+
+                    var linkNode = post.ChildNodes.FirstOrDefault(x => x.Attributes["href"] != null); // Find first child node with a link attribute (only "a" nodes here)
+
+                    // Add URL to the list
+                    postUrls.Add(ScrapePost(Creator.PartyDomain + linkNode?.Attributes["href"].Value, (page * 50) + count));
+                    count++;
+                }
+            }
+        }
+
+        return postUrls;
+    }
+
+    /// <summary>
+    /// Downloads the specified attachment. Basically just a public wrapper.
+    /// </summary>
+    /// <param name="attachment"></param>
+    /// <param name="parentFolder"></param>
+    /// <returns>A boolean representing the success of the download</returns>
+    public bool DownloadAttachment(Attachment attachment, string parentFolder)
+    {
+        if (attachment is { URL: not null, FileName: not null })
+        {
+            return DownloadContent(attachment.URL, parentFolder, attachment.FileName);
+        }
+        else
+        {
+            return false;
+        }
     }
 }
