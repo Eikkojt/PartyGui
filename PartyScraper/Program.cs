1b37265f938c5885ae58fe58fc50993f50e39a56
Branden Stober <67252380+BrandenStoberReal@users.noreply.github.com>
2024-01-06 22:37:34 -0500

    Code cleanup

--- PartyScraper/Program.cs
+++ PartyScraper/Program.cs
@@ -3,10 +3,10 @@ namespace KemonoScraperSharp_GUI
     internal static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
